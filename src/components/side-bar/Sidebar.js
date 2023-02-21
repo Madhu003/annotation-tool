27ae66f7fd3ddecc95d9ae85b23a2f25d1167701
madhu003 <madhusudanarya003@gmail.com>
2023-02-21 10:43:38 +0530

    down load functionality added

--- src/components/side-bar/Sidebar.js
+++ src/components/side-bar/Sidebar.js
@@ -6,10 +6,21 @@ import { useDispatch } from "react-redux";
 export const Sidebar = () => {
   const dispatch = useDispatch();
 
+  const dataURItoBlob = (dataURI) => {
+    var mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
+    var binary = atob(dataURI.split(",")[1]);
+    var array = [];
+    for (var i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], { type: mime });
+  };
+
   return (
     <div className="d-flex flex-column container">
       <div className="upload-section">
         <input
+          className="form-control my-4"
           type="file"
           onChange={(e) => {
             console.log(e);
@@ -27,7 +38,20 @@ export const Sidebar = () => {
       </div>
       <button
         className="btn btn-sm btn-primary"
-        onClick={() => dispatch({ type: "DOWNLOAD_IMAGE" })}
+        onClick={() => {
+          const canvas = document.querySelector("#myCanvas");
+
+          const baseUrl = canvas.toDataURL();
+          const blob = new Blob([baseUrl], { type: "plain/text" });
+
+          const imageURL = URL.createObjectURL(dataURItoBlob(baseUrl));
+          console.log(imageURL);
+          const aElement = document.createElement("a");
+          aElement.href = imageURL;
+          aElement.download = "test.png";
+          aElement.click();
+          // dispatch({ type: "DOWNLOAD_IMAGE" })
+        }}
       >
         Download Image
       </button>
--- src/reducers/sideBarReducer.js
+++ src/reducers/sideBarReducer.js
@@ -4,6 +4,8 @@ export const sideBarReducer = (state = inititalState, action) => {
   switch (action.type) {
     case "FILE_UPLOAD":
       return { ...state, uploadedFile: action.payload };
+    case "DOWNLOAD_IMAGE":
+      return { ...state };
     default:
       return state;
   }
